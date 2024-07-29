80fe27b5f724b635487334af544256dc3817abe1
Darren <darrengee.exe@gmail.com>
2024-07-29 16:22:52 -0400

    removed navbar items and made it grow horizontally, changed employee model, made physicianDirectory dynamic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import MedicineRegistry from './pages/MedicineRegistry';
-import EquipmentAvailability from './pages/EquipmentAvailability';
-import ShiftsManager from './pages/ShiftsManager';
-import InventoryManagement from './pages/InventoryManagement';
-import Workplaces from './pages/Workplaces';
-import PatientScheduler from './pages/PatientScheduler';
-import PatientProfile from './pages/PatientProfile';
 import PhysicianProfile from './pages/PhysicianProfile';
-import PatientDirectory from './pages/PatientDirectory';
 import PhysicianDirectory from './pages/PhysicianDirectory';
-import BedAvailability from './pages/BedAvailability';
 import SearchAppointment from './pages/SearchAppointment';
 import AppointmentDetails from './pages/AppointmentDetails';
+import './components/navbar.css'; 
 
+const Home = () => (
+  <div className="home-buttons">
+    <h2>Welcome to the Hospital Management App</h2>
+    <Link to="/physician-profile/666dce53a537510a1ef36e65" className="home-button">Sample Physician Profile</Link>
+    <Link to="/physician-directory" className="home-button">Physician Directory</Link>
+    <Link to="/search-appointment" className="home-button">Search Appointment</Link>
+    <Link to="/appointment-details" className="home-button">Appointment Details Sample</Link>
+  </div>
+);
 
 const App = () => {
   return (
@@ -21,39 +22,22 @@ const App = () => {
       <div>
         <h1>Hospital Management App</h1>
         <nav>
-          <ul>
-            <li><Link to="/medicine-registry">Medicine Registry</Link></li>
-            <li><Link to="/shifts-manager">Shifts Manager</Link></li>
-            <li><Link to="/inventory-management">Inventory Management</Link></li>
-            <li><Link to="/workplaces">Workplaces</Link></li>
-            <li><Link to="/patient-scheduler">Patient Scheduler</Link></li>
-            <li><Link to="/patient-profile/666b951274a8b4f99a5a9ef5">Sample Patient Profile</Link></li>
+          <ul className="navbar">
+            <li><Link to="/">Home</Link></li>
             <li><Link to="/physician-profile/666dce53a537510a1ef36e65">Sample Physician Profile</Link></li>
-            <li><Link to="/patient-directory">Patient Directory</Link></li>
-            <li><Link to="/Physician-directory">Physician Directory</Link></li>
-            <li><Link to="/bed-availability">Bed Availability</Link></li>
-            <li><Link to="/equipment-availability">Equipment Availability</Link></li>
+            <li><Link to="/physician-directory">Physician Directory</Link></li>
             <li><Link to="/search-appointment">Search Appointment</Link></li>
             <li><Link to="/appointment-details">Appointment Details Sample</Link></li>
           </ul>
         </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/physician-profile/:id" element={<PhysicianProfile />} />
+          <Route path="/physician-directory" element={<PhysicianDirectory />} />
+          <Route path="/search-appointment" element={<SearchAppointment />} />
+          <Route path="/appointment-details" element={<AppointmentDetails />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="/medicine-registry" element={<MedicineRegistry />} />
-        <Route path="/equipment-availability" element={<EquipmentAvailability />} />
-        <Route path="/shifts-manager" element={<ShiftsManager />} />
-        <Route path="/inventory-management" element={<InventoryManagement />} />
-        <Route path="/workplaces" element={<Workplaces />} />
-        <Route path="/patient-scheduler" element={<PatientScheduler />} />
-        <Route path="/patient-profile/:id" element={<PatientProfile />} />
-        <Route path="/physician-profile/:id" element={<PhysicianProfile />} />
-        <Route path="/patient-directory" element={<PatientDirectory />} />
-        <Route path="/physician-directory" element={<PhysicianDirectory />} />
-        <Route path="/bed-availability" element={<BedAvailability />} />
-        <Route path="/search-appointment" element={<SearchAppointment />} />
-        <Route path="/appointment-details" element={<AppointmentDetails />} />
-
-      </Routes>
     </Router>
   );
 };
